c34b3f461280c8e5b6236e279e1a22475a05ca4d
chanxl <1060177942@qq.com>
2025-05-12 10:44:53 +0800

    增加 详细实现线性回归 生成数据集 部分代码

--- learn_LinearNeuralNetword.py
+++ learn_LinearNeuralNetword.py
@@ -3,6 +3,7 @@ import torch
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 ''' 矢量化加速例子 开始 '''
 # # 实例化两个全为1的10000维向量
@@ -50,5 +51,32 @@ import matplotlib.pyplot as plt
 # plt.show()
 ''' 正态分布与平方缺失 结束 '''
 
+
+''' 线性回归的从零开始实现 开始 '''
+# 数据集中有1000个样本，每个样本包含从标准正态分布中采样的2个特征
+'''
+# 生成 ”符合线性关系 y=Xw+b+噪声“ 的合成数据集
+# w: 权重向量（决定线性关系的斜率）
+# b: 偏置项（决定线性关系的截距）
+# num_examples: 要生成的样本数量
+'''
+def synthetic_data(w, b, num_examples):  #@save
+    """生成y=Xw+b+噪声"""
+    # 生成一个形状为 (num_examples, len(w)) 的矩阵,每个元素从均值为0、标准差为1的正态分布中随机采样
+    X = torch.normal(0, 1, (num_examples, len(w)))
+    y = torch.matmul(X, w) + b          # 计算线性部分 Xw + b
+    y += torch.normal(0, 0.01, y.shape) # 计算线性部分 Xw + b
+    return X, y.reshape((-1, 1))        # 返回特征矩阵X和标签向量y，返回特征矩阵X和标签向量y
+
+# 定义真实的权重 w = [2, -3.4] 和偏置 b = 4.2
+true_w = torch.tensor([2, -3.4])
+true_b = 4.2
+
+# features: 形状为 (1000, 2) 的矩阵
+# labels: 形状为 (1000, 1) 的向量
+features, labels = synthetic_data(true_w, true_b, 1000) # 生成1000个样本
+print('features:', features[0],'\nlabel:', labels[0])
+
+''' 线性回归的从零开始实现 结束 '''
 
 
