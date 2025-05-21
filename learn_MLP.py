fd7f2670ed824f8d44f6624821ac41b3ea2f1d78
chanxl <1060177942@qq.com>
2025-05-21 17:17:02 +0800

    补充学习代码：多层感知机，激活函数部分

--- learn_MLP.py
+++ learn_MLP.py
@@ -1,6 +1,6 @@
 import torch
+from torch import nn
 import matplotlib.pyplot as plt
-
 
 # 绘图函数
 def plot(x, y, xlabel, ylabel, figsize=(5, 2.5)):
@@ -14,20 +14,109 @@ def plot(x, y, xlabel, ylabel, figsize=(5, 2.5)):
 
 # 生成从-8.0到7.9（不包括8.0）的列表，步长为0.1，需要梯度
 x = torch.arange(-8.0, 8.0, 0.1, requires_grad=True)
-# 通过ReLU函数激活（将所有负值置为0，正值保持不变）
-y = torch.relu(x)
-# detach() 用于从计算图中分离张量，以便转换为numpy数组进行绘图
-plot(x.detach(), y.detach(), 'x', 'ReLU(x)', figsize=(5, 2.5))
-# 绘图展示了ReLU函数的典型形状：在x<0时输出0，x≥0时输出x
 
-# 绘制 ReLU函数的导数
-# 执行反向传播
-# torch.ones_like(x)创建一个与x形状相同的全1张量，作为反向传播的梯度输入。假设y的输出对后续计算的贡献都是1
-# retain_graph=True 在反向传播后保留计算图，以便后续还可再进行反向传播。在多次反向传播或计算高阶导数时很有用
-y.backward(torch.ones_like(x), retain_graph=True)
+def learn_ActivationFunction():
+    ''' 绘制ReLU函数 '''
+    # 通过ReLU函数激活（将所有负值置为0，正值保持不变）
+    y = torch.relu(x)
+    # detach() 用于从计算图中分离张量，以便转换为numpy数组进行绘图
+    # plot(x.detach(), y.detach(), 'x', 'ReLU(x)', figsize=(5, 2.5))
+    # 绘图展示了ReLU函数的典型形状：在x<0时输出0，x≥0时输出x
 
-# 绘图  x.grad是x的梯度，通过反向传播计算得到
-plot(x.detach(), x.grad, 'x', 'grad of relu', figsize=(5, 2.5))
+    # 绘制 ReLU函数的导数
+    # 执行反向传播
+    # torch.ones_like(x)创建一个与x形状相同的全1张量，作为反向传播的梯度输入。假设y的输出对后续计算的贡献都是1
+    # retain_graph=True 在反向传播后保留计算图，以便后续还可再进行反向传播。在多次反向传播或计算高阶导数时很有用
+    y.backward(torch.ones_like(x), retain_graph=True)
 
+    # 绘图  x.grad是x的梯度，通过反向传播计算得到
+    # plot(x.detach(), x.grad, 'x', 'grad of relu', figsize=(5, 2.5))
+
+
+    ''' 绘制sigmoid函数 '''
+    y = torch.sigmoid(x)
+    # plot(x.detach(), y.detach(), 'x', 'sigmoid(x)', figsize=(5, 2.5))
+
+    # 绘制 sigmoid函数的导数
+    x.grad.data.zero_() # 清除以前的梯度
+    y.backward(torch.ones_like(x),retain_graph=True)
+    # plot(x.detach(), x.grad, 'x', 'grad of sigmoid', figsize=(5, 2.5))
+
+
+    ''' 绘制tanh函数 '''
+    y = torch.tanh(x)
+    plot(x.detach(), y.detach(), 'x', 'tanh(x)', figsize=(5, 2.5))
+
+    # 绘制 tanh函数的导数
+    x.grad.data.zero_() # 清除以前的梯度
+    y.backward(torch.ones_like(x),retain_graph=True)
+    plot(x.detach(), x.grad, 'x', 'grad of tanh', figsize=(5, 2.5))
+
+# learn_ActivationFunction()
+
+def visual_ActivationFunction():
+    ''' 所有激活函数及其导数图像画在一个图里 '''
+    plt.figure(figsize=(10, 5))  # 创建一个新的图形对象
+
+    # 添加rule及其导数
+    plt.subplot(231)
+    y = torch.relu(x)
+    plt.plot(x.detach().numpy(), y.detach().numpy(), 'b-', linewidth=1)
+    plt.xlabel('x')
+    plt.ylabel('ReLU(x)')
+    plt.grid(True)
+
+    plt.subplot(234)
+    x.grad.data.zero_() # 清除以前的梯度
+    y.backward(torch.ones_like(x), retain_graph=True)
+    plt.plot(x.detach().numpy(), x.grad.numpy(), 'b-', linewidth=1)
+    plt.xlabel('x')
+    plt.ylabel('grad of relu')
+    plt.grid(True)
+
+
+    # 添加sigmoid及其导数
+    plt.subplot(232)
+    y = torch.sigmoid(x)
+    plt.plot(x.detach().numpy(), y.detach().numpy(), 'b-', linewidth=1)
+    plt.xlabel('x')
+    plt.ylabel('sigmoid(x)')
+    plt.grid(True)
+
+    plt.subplot(235)
+    x.grad.data.zero_() # 清除以前的梯度
+    y.backward(torch.ones_like(x), retain_graph=True)
+    plt.plot(x.detach().numpy(), x.grad.numpy(), 'b-', linewidth=1)
+    plt.xlabel('x')
+    plt.ylabel('grad of sigmoid')
+    plt.grid(True)
+
+
+    # 添加tanh及其导数
+    plt.subplot(233)
+    y = torch.tanh(x)
+    plt.plot(x.detach().numpy(), y.detach().numpy(), 'b-', linewidth=1)
+    plt.xlabel('x')
+    plt.ylabel('tanh(x)')
+    plt.grid(True)
+
+    plt.subplot(236)
+    x.grad.data.zero_() # 清除以前的梯度
+    y.backward(torch.ones_like(x), retain_graph=True)
+    plt.plot(x.detach().numpy(), x.grad.numpy(), 'b-', linewidth=1)
+    plt.xlabel('x')
+    plt.ylabel('grad of tanh')
+    plt.grid(True)
+
+    plt.tight_layout()  # 自动调整子图参数，使之填充整个图形区域
+    plt.show()
+
+# visual_ActivationFunction()
+
+
+''' 多层感知机的从零开始实现 开始 '''
+batch_size = 256
+
+''' 多层感知机的从零开始实现 结束 '''
 
 
