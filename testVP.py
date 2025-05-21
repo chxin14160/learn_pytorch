a7248b4819b569b3672e111dcea4e57d1ee3d41c
chanxl <1060177942@qq.com>
2025-05-21 11:58:54 +0800

    增加代码文件：读取txt文件中点集，用open3d显示点云

---
+++ testVP.py
@@ -0,0 +1,18 @@
+import open3d as o3d
+import numpy as np
+
+# points_data = np.loadtxt("E:/vsproject/勋仪交接/标克艾芬达暖水管/测量主管尺寸/vp/11pnts.txt", delimiter=",", dtype=np.float32)
+points_data = np.loadtxt("E:/vsproject/勋仪交接/标克艾芬达暖水管/测量主管尺寸/vp/11pnts.txt", delimiter=",")
+print('shape: ', points_data.shape)
+print('data type: ', points_data.dtype)
+
+points_data[:,2] = points_data[:,2] * 21 # 将所有行的第三列乘以 21
+
+# points_data = np.random.rand(100, 3)  # 示例数据：100 个随机 3D 点
+colors_data = np.random.rand(100, 3)  # 100 个随机 RGB 颜色数组
+
+pcd = o3d.geometry.PointCloud() # 创建一个空的点云对象
+pcd.points = o3d.utility.Vector3dVector(points_data[:, :3]) # 从NumPy数组points_data中分配3D点（取前3列作为 x,y,z 坐标）
+pcd.colors = o3d.utility.Vector3dVector(colors_data)  # 分配颜色信息
+o3d.visualization.draw_geometries([pcd]) # 可视化点云
+
