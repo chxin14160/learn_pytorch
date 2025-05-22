605a76a0030f3416327dd729a32370be7c9b7e53
chanxl <1060177942@qq.com>
2025-05-22 17:49:48 +0800

    增加内容

--- testVP.py
+++ testVP.py
@@ -16,3 +16,10 @@ pcd.points = o3d.utility.Vector3dVector(points_data[:, :3]) # 从NumPy数组poin
 pcd.colors = o3d.utility.Vector3dVector(colors_data)  # 分配颜色信息
 o3d.visualization.draw_geometries([pcd]) # 可视化点云
 
+
+# # 创建alpha shape并进行mesh化
+# alpha = 0.015  # 设置alpha值
+# mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, alpha)
+#
+# # 可视化网格化后的结果
+# o3d.visualization.draw_geometries([mesh], window_name="网格化结果")
