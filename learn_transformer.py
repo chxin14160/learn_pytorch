cebb3dbb734f576a0825fdb9a2ebaa1bf61e4805
chanxl <1060177942@qq.com>
2025-08-27 15:58:26 +0800

    增加功能函数代码：注意力机制-显示矩阵热图

--- learn_transformer.py
+++ learn_transformer.py
@@ -1,6 +1,11 @@
 import torch
 import common
 
-
+# 仅当查询和键相同时，注意力权重为1，否则为0
+# torch.eye(10): 生成一个 10x10 的单位矩阵（对角线为 1，其余为 0）
+# .reshape((1,1,10,10)): 调整形状为(batch_size=1,num_heads=1,seq_len=10,seq_len=10)
+# 模拟一个单头注意力机制的权重矩阵（Queries × Keys）
+attention_weights = torch.eye(10).reshape((1, 1, 10, 10))
+common.show_heatmaps(attention_weights, xlabel='Keys', ylabel='Queries')
 
 
