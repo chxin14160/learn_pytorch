ed487ecedc1174bc6d47e951499b162fc99cf00c
chanxl <1060177942@qq.com>
2025-08-27 18:07:27 +0800

    增加代码：可视化.eye()生成的 对角线为1其余为0的 10*10单位矩阵

--- learn_transformer.py
+++ learn_transformer.py
@@ -1,6 +1,8 @@
 import torch
 import common
 
+print(f"10x10 的单位矩阵（对角线为 1，其余为 0）：\n{torch.eye(10)}")
+
 # 仅当查询和键相同时，注意力权重为1，否则为0
 # torch.eye(10): 生成一个 10x10 的单位矩阵（对角线为 1，其余为 0）
 # .reshape((1,1,10,10)): 调整形状为(batch_size=1,num_heads=1,seq_len=10,seq_len=10)
